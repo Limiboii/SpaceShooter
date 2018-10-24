ef715fe9ced79a398a4541c3bbb8b625791edf99
Liam Linnarsson <Liam.salllinnarsson@elev.ga.lbs.se>
2018-10-24 13:58:16 +0200

    Scrolling background fixed
    I fixed the scrolling background

---
+++ Assets/Scripts/Scroller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroller : MonoBehaviour
+{
+    private float scalar = 1f;
+    private Material mat;
+    
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+    
+    void Update()
+    {
+        mat.mainTextureOffset = new Vector2(Time.time * scalar, 0);
+        
+    }
+}
