1106db169c1296f180438eb5c2b2b8e26315192c
Liam Linnarsson <Liam.salllinnarsson@elev.ga.lbs.se>
2018-12-05 15:24:17 +0100

    Explosion

---
+++ Assets/Scripts/KillExplosion.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillExplosion : MonoBehaviour
+{
+    void Update()
+    {
+        if (Time.deltaTime >= 0.12)
+            Destroy(gameObject);
+    }
+}
